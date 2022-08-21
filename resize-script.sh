47e2a993637da4bd709897bd9e1a600c230da5f9
Tesfahunegn Minwuyelet Mengistu <47448502+Tesfahunegn369@users.noreply.github.com>
2022-08-21 11:03:36 +0300

    Add files via upload

---
+++ resize-script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#simple script for resizing images in all class directories
+#also reformats everything from whatever to png
+# Originaly from JPG-PNG-to-MNIST-NN-Format Project and modified by tikroute
+
+if [ `ls valid/*.jpg 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in valid/*.jpg; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+    rm "$file"
+  done
+fi
+
+if [ `ls valid/*/*.png 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in valid/*/*.png; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+  done
+fi
+
+'''if [ `ls test/*/*.jpg 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in test/*/*.jpg; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+    rm "$file"
+  done
+fi
+
+if [ `ls test/*/*.png 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in test/*/*.png; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+  done
+fi
+if [ `ls valid/*/*.jpg 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in valid/*/*.jpg; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+    rm "$file"
+  done
+fi
+
+if [ `ls valid/*/*.png 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in valid/*/*.png; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+  done
+fi'''
